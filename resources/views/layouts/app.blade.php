0a8c13b760350b7aa45f7d801bcfe51087b5be93
kellyandra <ms.kchristian86@gmail.com>
2024-04-10 23:14:47 -0400

    navbar temporary color change.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,14 +3,16 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>COMP3385 - Assignment</title>
+    <title>FTL</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+   
+
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<nav class="navbar navbar-expand-lg navbar-dark" style="background-image: linear-gradient(to right, #0375d296 , #003a6a96 );" >
     <div class="container">
-        <a class="navbar-brand" href="#">COMP3385</a>
+        <a class="navbar-brand" href="#">FTL</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -22,9 +24,9 @@
                 <li class="nav-item">
                     <a class="nav-link" href="{{ url('/about') }}">About</a>
                 </li>
-                <li class="nav-item">
+                <!-- <li class="nav-item">
                     <a class="nav-link" href="{{ url('/about') }}">About</a>
-                </li>
+                </li> -->
                 @if (!Auth::check())
                 <li class="nav-item">
                     <a class="nav-link" href="{{ url('/login') }}">Login</a>
@@ -51,3 +53,5 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
+
+
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,10 +7,10 @@
         </div>
     @endif
     <div class="px-4 py-5 my-5 text-center bg-body-tertiary rounded-3">
-        <img class="img-fluid" src="{{ asset('images/UWI-Wordmark.webp') }}" alt="UWI Wordmark" width="300" />
+        <img class="img-fluid" src="{{ asset('images/ftl.webp') }}" alt="ftl" width="300" />
         <h1 class="display-5 fw-bold text-body-emphasis">Welcome</h1>
         <div class="col-lg-6 mx-auto">
-            <p class="lead mb-4">You've successfully loaded up the Laravel starter template for this assignment.</p>
+            <p class="lead mb-4">Welcome to Faster than Light Banking!</p>
         </div>
     </div>
 @endsection
